6cf5d47c7c59fcfbf414824992409e320b582a7e
Sean Beardsley <sixer601@gmail.com>
2023-02-02 12:42:52 -0500

    Created Skeleton of Program

--- Homework3/smbeardsley-homework3.cc
+++ Homework3/smbeardsley-homework3.cc
@@ -1,8 +1,52 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+// PRE: 
+// POST: 
+void inputData(istream &pInputFile) {
+    
+
+}
+
+// PRE: 
+// POST: 
+void createChildProcesses() {
+
+
+}
+
+// PRE: 
+// POST: 
+void handleJobs() {
+
+
+}
+
+// PRE: 
+// POST: 
 int main(int argc, char **argv) {
-    cout << "Welcome to my planit 2" << endl;
+    
+    if (argc != 2) {
+        cout << "Invalid number of arguments." << endl;
+    }
+    else {
+
+        ifstream inputFile(argv[1]);
+
+        // Input file data into shared memory
+        inputData(inputFile);
+
+        // Create child processes
+        createChildProcesses();
+
+        // Begin handling chunks/jobs
+        handleJobs();
+
+    }
+    
+
     return(0);
+
 }
