fb2152fec945d0df10702d8a67f43d41977723d1
sixer601 <sixer601@gmail.com>
2023-02-05 18:02:02 -0500

    Added merge and mergesort to helper functions

--- Homework3/HelperFunctions.cc
+++ Homework3/HelperFunctions.cc
@@ -1,6 +1,5 @@
 #include "HelperFunctions.h"
-#include <iostream>
-#include <fstream>
+#include "IntArray.h"
 
 using namespace std;
 
@@ -20,6 +19,66 @@ int ceiling(int a, int b)
 	return(result);
 }
 
+void merge(IntArray pIntArray, int p, int q, int r)
+{
+	int i = p;
+	int j = q;
+	int k = 0;
+	int * temp = new int[r - p];
+	for (int a = p; a < r; a++)
+	{
+		temp[a] = pIntArray.getNthIntInArray(a);
+	}
+	while (i < q && j < r)
+	{
+		if (pIntArray.getNthIntInArray(i) < pIntArray.getNthIntInArray(j))
+		{
+			temp[k] = pIntArray.getNthIntInArray(i);
+			i++;
+		}
+		else
+		{
+			temp[k] = pIntArray.getNthIntInArray(j);
+			j++;
+		}
+		k++;
+	}
+	if (i == q)
+	{
+		while(j < r)
+		{
+			temp[k] = pIntArray.getNthIntInArray(j);
+			j++;
+			k++;
+		}
+	}
+	else if (j == r)
+	{
+		while(i < q)
+		{
+			temp[k] = pIntArray.getNthIntInArray(i);
+			i++;
+			k++;
+		}
+	}
+	for (int b = p; b < r; b++)
+	{
+		pIntArray.replaceInt(b, temp[b]);
+	}
+}
+
+// PRE: 
+// POST: 
+void mergeSort(IntArray pIntArray, int p, int r)
+{
+	if (p != r)
+	{
+		int q = ((p + r) / 2);
+		mergeSort(pIntArray, p, (q - 1));
+		mergeSort(pIntArray, q, r);
+		merge(pIntArray, p, q, r);
+	}
+}
 
 
 
--- Homework3/Homework3.cc
+++ Homework3/Homework3.cc
@@ -16,30 +16,14 @@ using namespace std;
 // POST:
 void createJobBoard(bool debugMode, key_t jobKey, int &numChunks, int &jobBoardSpaceRequired)
 {
-	if (debugMode)
-	{
-		cout << "Entered createJobBoard" << endl;
-	}
-
 	int jobBoard_mem_id;
 
 	char *shm, *jobs;
 
 	int numJobs = numChunks + ceiling(numChunks, 2);
 
-	if (debugMode)
-	{
-		cout << "Number of jobs to handle: " << numJobs << endl;
-	}
-
 	jobBoardSpaceRequired = (sizeof(char) * numJobs);
 
-	if (debugMode)
-	{
-		cout << "Space Required for Job Board: " << jobBoardSpaceRequired << endl;
-	}
-
-	// Create shared memory segment
 	jobBoard_mem_id = shmget(jobKey, jobBoardSpaceRequired, IPC_CREAT | 0666);
 	if (jobBoard_mem_id < 0)
 	{
@@ -47,7 +31,6 @@ void createJobBoard(bool debugMode, key_t jobKey, int &numChunks, int &jobBoardS
 	}
 	else
 	{
-		// Attach segment to our data space
 		shm = (char *)shmat(jobBoard_mem_id, NULL, 0);
 		if (shm == (char *)-1)
 		{
@@ -74,31 +57,14 @@ void createJobBoard(bool debugMode, key_t jobKey, int &numChunks, int &jobBoardS
 // POST:
 void createSharedMemory(bool debugMode, IntArray pIntArray, key_t key, int &informationSpaceRequired, int &numChunks)
 {
-	if (debugMode)
-	{
-		cout << "Entered createSharedMemory." << endl;
-	}
-
 	int shared_mem_id;
 
 	int *shm, *information;
 
 	numChunks = ceiling(pIntArray.getContentLength(), CHUNKSIZE);
 
-	if (debugMode)
-	{
-		cout << "Number of chunks to handle: " << numChunks << endl;
-	}
-
-	// TODO: Calculate space required for intArray
 	informationSpaceRequired = ((sizeof(int) * (pIntArray.getContentLength())));
 
-	if (debugMode)
-	{
-		cout << "Space Required: " << informationSpaceRequired << endl;
-	}
-
-	// Create shared memory segment
 	shared_mem_id = shmget(key, informationSpaceRequired, IPC_CREAT | 0666);
 	if (shared_mem_id < 0)
 	{
@@ -106,7 +72,6 @@ void createSharedMemory(bool debugMode, IntArray pIntArray, key_t key, int &info
 	}
 	else
 	{
-		// Attach segment to our data space
 		shm = (int *)shmat(shared_mem_id, NULL, 0);
 		if (shm == (int *)-1)
 		{
@@ -116,7 +81,6 @@ void createSharedMemory(bool debugMode, IntArray pIntArray, key_t key, int &info
 		{
 			information = shm;
 
-			// Store data in memory for other processes to read
 			for (int j = 0; j < pIntArray.getContentLength(); j++)
 			{
 				information[j] = pIntArray.getNthIntInArray(j);
@@ -145,29 +109,14 @@ void inputData(bool debugMode, istream &pInputFile, key_t key, int &spaceRequire
 		cout << "Entered inputData." << endl;
 	}
 
-	// Input data from file
 	IntArray data;
 	while (pInputFile.peek() != EOF)
 	{
-		// ASSERT:
 		int datum;
 		pInputFile >> datum;
 		data.addInt(datum);
 	}
 
-	// Debug Mode Output
-	if (debugMode)
-	{
-		cout << "data: ";
-		for (int i = 0; i < data.getContentLength(); i++)
-		{
-			// ASSERT:
-			cout << data.getNthIntInArray(i) << " ";
-		}
-		cout << endl;
-	}
-
-	// Store intArray in shared memory
 	createSharedMemory(debugMode, data, key, spaceRequired, numChunks);
 }
 
@@ -175,15 +124,9 @@ void inputData(bool debugMode, istream &pInputFile, key_t key, int &spaceRequire
 // POST:
 int createChildProcesses(bool debugMode, int neededChildrenNum, int &childNum)
 {
-	if (debugMode)
-	{
-		cout << getpid() << " entered createChildProcesses which will make " << neededChildrenNum << " processes." << endl;
-	}
-
 	bool child = false;
 	while (childNum < neededChildrenNum && !child)
 	{
-		// ASSERT:
 		pid_t childPID = fork();
 		if (childPID == -1)
 		{
@@ -191,13 +134,11 @@ int createChildProcesses(bool debugMode, int neededChildrenNum, int &childNum)
 		}
 		else if (childPID == 0)
 		{
-			// Child Process
 			childNum++;
 			child = true;
 		}
 		else
 		{
-			// Parent Process
 			childNum++;
 		}
 	}
@@ -214,56 +155,27 @@ void handleJobs(bool debugMode, int childNum, int numChunks, int numProcesses, k
 {
 	int jobBoardSHMid, infoSHMid;
 
-	if (debugMode)
-	{
-		cout << getpid() << " Entered handleJobs with key: " << jobKey << endl;
-	}
-
 	char *jobSHM;
 	int *infoSHM;
 
-	/*
-	 * Locate the segment for the job board.
-	 */
 	if ((jobBoardSHMid = shmget(jobKey, jobBoardSpaceRequired, 0666)) < 0)
 	{
 		perror("shmget");
 	}
 	else
 	{
-		/*
-		 * Now we attach the segment for the job board to our data space.
-		 */
 		if ((jobSHM = (char *)shmat(jobBoardSHMid, NULL, 0)) == (char *)-1)
 		{
 			perror("shmat");
 		}
 		else
 		{
-			if (debugMode)
-			{
-				cout << "Jobs: ";
-
-				for (int i = 0; i < (jobBoardSpaceRequired / sizeof(char)); i++)
-				{
-					cout << jobSHM[i] << " ";
-				}
-
-				cout << endl;
-			}
-
-			/*
-			 * Locate the segment for the information.
-			 */
 			if ((infoSHMid = shmget(infoKey, infoSpaceRequired, 0666)) < 0)
 			{
 				perror("shmget");
 			}
 			else
 			{
-				/*
-				 * Now we attach the segment for the information to our data space.
-				 */
 				if ((infoSHM = (int *)shmat(infoSHMid, NULL, 0)) == (int *)-1)
 				{
 					perror("shmat");
@@ -302,58 +214,26 @@ void handleJobs(bool debugMode, int childNum, int numChunks, int numProcesses, k
 int main(int argc, char **argv)
 {
 
-	if (argc != 4)
+	if (argc != 3)
 	{
 		cout << "Invalid number of arguments." << endl;
 	}
 	else
 	{
 		bool debugMode;
-		int numChildren = stoi(argv[1]);
-		ifstream inputFile(argv[2]);
-		int thirdArg = stoi(argv[3]);
+		int numChildren = stoi(argv[2]);
+		ifstream inputFile(argv[1]);
 		key_t informationKey = 914615;
 		key_t jobKey = 10152;
 		int informationSpaceRequired = 0;
 		int jobBoardSpaceRequired = 0;
 		int numChunks = 0;
 		int childNum = 0;
-		if (thirdArg == 1 || thirdArg == 0)
-		{
-			if (thirdArg == 1)
-			{
-				debugMode = true;
-			}
-			else
-			{
-				debugMode = false;
-			}
 
-			if (debugMode)
-			{
-				cout << "number of children required: " << numChildren << endl;
-				cout << "information key: " << informationKey << endl;
-				cout << "job key: " << jobKey << endl;
-			}
-
-			// Input file data into shared memory
-			inputData(debugMode, inputFile, informationKey, informationSpaceRequired, numChunks);
-
-			cout << "Numbers in input: " << (informationSpaceRequired / sizeof(int)) << endl;
-
-			// Create Job board to assign jobs to children
-			createJobBoard(debugMode, jobKey, numChunks, jobBoardSpaceRequired);
-
-			// Create child processes
-			createChildProcesses(debugMode, numChildren, childNum);
-
-			// Begin handling chunks/jobs
-			handleJobs(debugMode, childNum, numChunks, (numChildren + 1), jobKey, informationKey, jobBoardSpaceRequired, informationSpaceRequired);
-		}
-		else
-		{
-			cout << "Enter a 1 or 0 for debug mode." << endl;
-		}
+		inputData(debugMode, inputFile, informationKey, informationSpaceRequired, numChunks);
+		createJobBoard(debugMode, jobKey, numChunks, jobBoardSpaceRequired);
+		createChildProcesses(debugMode, numChildren, childNum);
+		handleJobs(debugMode, childNum, numChunks, (numChildren + 1), jobKey, informationKey, jobBoardSpaceRequired, informationSpaceRequired);
 	}
 
 	return (0);
